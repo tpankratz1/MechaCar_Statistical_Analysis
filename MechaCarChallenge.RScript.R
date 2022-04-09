23982b46e479c1c8ddeab6e508683c7b712de784
tpankratz1 <96349572+tpankratz1@users.noreply.github.com>
2022-04-09 11:04:38 -0500

    updated R file

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -1,6 +1,6 @@
 library(dplyr)
 
 # Miles Per Gallon
-cars_df <- read.csv('MechaCar_mpg.csv',stringsAsFactors = F) #read in dataset
-summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, cars_df))
+mechacars_df <- read.csv('MechaCar_mpg.csv',stringsAsFactors = F) #read in dataset
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, mechacars_df))
 
