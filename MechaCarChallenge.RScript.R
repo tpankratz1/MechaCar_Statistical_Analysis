4063147a2e6fafc2ac8bd3ca6ad9f07b90540942
tpankratz1 <96349572+tpankratz1@users.noreply.github.com>
2022-04-10 15:47:46 -0500

    Updated R file

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -3,6 +3,7 @@ library(dplyr)
 # Miles Per Gallon Multiple Linear Regression
 mechacar_df <- read.csv('MechaCar_mpg.csv',stringsAsFactors = F) #read in dataset
 
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, mechacar_df)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, mechacar_df))
 
 # Suspension Coils Tables
